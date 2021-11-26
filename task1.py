e31ff24eb707f04d3bc770b89bd97d5d4299a4c8
#1 wabungus fan <90716044+ye11owbucket@users.noreply.github.com>
2021-11-26 14:43:14 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,8 +9,33 @@ Example:
 assert find('apple') == 0
 assert find('fish') == 5
 '''
+input1 = str(input("input: "))
+def mayonaise(a):
+    x = len(a)
+    if x == 0:
+        return "no bueno"
+    if x > 1:
+        mayo = (x + " stocks have that symbol")
+        return mayo
+    if x == 1:
+        return a
+
 def find(needle):
-    pass
+    pyf1 = "task02.csv"
+    list1 = []
+    count = 0 
+    for haystack in pyf1:
+        if needle in haystack:
+            x = haystack.strip()
+            data = x.split(" ")
+            list1.append(data)
+            count + count + 1
+    mayonaise(list1)
+
+find(input1)
+            
+
+
 
 
 if __name__ == "__main__":
